444cd4a5ff4f60b1787afe8668ef5e913738563c
AutomationTesting0612 <akhil.sharma0612@gmail.com>
2023-02-21 14:41:22 +0530

    added java document

---
+++ src/test/java/utility/Utility.java
@@ -0,0 +1,22 @@
+package utility;
+
+import cucumber.annotation.After;
+import cucumber.annotation.Before;
+
+
+public class Utility {
+
+    @Before
+    public void openBrowser() {
+
+    }
+
+    @After
+    public void closeBrowser() {
+
+    }
+}
+
+
+
+
