be3f4a1716afc792cb74d148b294cd7a5992bdda
AutomationTesting0612 <automationtesting0612@gmail.com>
2025-04-10 15:59:09 +0530

    refactored classes

--- src/main/java/com/automation/SpringapiApplication.java
+++ src/main/java/com/automation/SpringapiApplication.java
@@ -94,91 +94,91 @@ public class SpringapiApplication {
     }
 //    @PostConstruct
 
-        @KafkaListener(topics = "QATransaction_API_PAYLOAD", groupId = "your-group-id")
-        public void consumeMessage(String message) throws JsonProcessingException {
-
-                scenario = objectMapper.readValue(message, ScenarioMain.class);
-                receivedMessages.add(scenario);
-                setupExtentReports();
-                doHttpCall(scenario);
-
-        }
-
-        private void doHttpCall(ScenarioMain scenario) throws JsonProcessingException {
-            // Creating headers for the HTTP request
-            HttpHeaders headers = new HttpHeaders();
-            ResponseEntity<String> response = null;
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            if (header != null) {
-                header.forEach(headers::set);
-            }
-            HttpMethod method;
-            for (DataList dataList : scenario.getData_list()) {
-                method= HttpMethod.valueOf(dataList.getMapping_type());
-                featureName = dataList.getScenario().getName();
-                operationType = dataList.getMapping_type();
-                header = dataList.getHeader();
-                test = extent.createTest("Feature: " + featureName)
-                        .assignCategory("API Testing")
-                        .assignAuthor("Automation Team");
-                for (DataSet dataset : dataList.getScenario().getDatasets()) {
-                    Map<String, Object> requestBody = dataset.getRequest_body() != null
-                            ? dataset.getRequest_body()
-                            : new HashMap<>();
-                    HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
-
-                    String expectedStatus = dataset.getDesired_status() != null ? dataset.getDesired_status() : "";
-                    String expectedResponse = dataset.getDesired_outcome() != null ? dataset.getDesired_outcome() : "{}";
-                    String endpoint = dataList.getBase_url()+dataList.getEndPoint();
-                    // Converting actual and expected responses to Maps for easy comparison
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    test.info(endpoint);
-                    test.info(String.valueOf(method));
-
-//                    Map<String, Object> expectedResponseMap = objectMapper.readValue(expectedResponse, Map.class);
-//                    String expectedResponseBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(expectedResponse);
-//                   String finalExpectedResponse =  objectMapper.writeValueAsString(expectedResponseBody);
-
-                    try {
-                        response = restTemplate.exchange(endpoint, method, entity, String.class);
-                        String actualResponse = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response.getBody());
+//        @KafkaListener(topics = "QATransaction_API_PAYLOAD", groupId = "your-group-id")
+//        public void consumeMessage(String message) throws JsonProcessingException {
 //
-//                        Map<String, Object> actualResponseMap = objectMapper.convertValue(response.getBody(), Map.class);
-
-//                        if (response.getStatusCodeValue() == Integer.parseInt(dataset.getDesired_status()) &&
-//                                actualResponse.equals(expectedResponse)) {
-                            if (response.getStatusCodeValue() == Integer.parseInt(dataset.getDesired_status())) {
-                            test.pass("✅ Status Matched: " + expectedStatus);
-                        } else {
-                            test.fail("⚠️ Unexpected Status: Expected " + dataset.getDesired_status() + " but got " + response.getStatusCodeValue());
-//                            test.fail("Unexpected Status: Expected Response Body " + dataset.getDesired_status() + " but got " + response.getBody());
-                        }
-
-                    } catch (Exception e) {
-                        // Log the failure and continue with the next dataset
-                        test.fail("❌ API call failed for " + dataset.getRequest_body() + ": " + e.getMessage());
-                    }
-                }
-    }
-            extent.flush();
-
-
-            try {
-                String htmlContent = new String(Files.readAllBytes(Paths.get("APIReport.html")), StandardCharsets.UTF_8);
-                scenario.setReport(htmlContent);
-            } catch (IOException e) {
-                System.err.println("Failed to read Extent Report: " + e.getMessage());
-            }
-
-         String finalMessage=  objectMapper.writeValueAsString(scenario);
-
-                String responseTopic = "QATransaction_API_REPORT";
-                kafkaTemplate.send(responseTopic, finalMessage);
-                System.out.println("Published response to Kafka topic: " + responseTopic);
-
-            File file = new File("APIReport.html");
-            file.delete();
-        }
+//                scenario = objectMapper.readValue(message, ScenarioMain.class);
+//                receivedMessages.add(scenario);
+//                setupExtentReports();
+//                doHttpCall(scenario);
+//
+//        }
+//
+//        private void doHttpCall(ScenarioMain scenario) throws JsonProcessingException {
+//            // Creating headers for the HTTP request
+//            HttpHeaders headers = new HttpHeaders();
+//            ResponseEntity<String> response = null;
+//            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+//            if (header != null) {
+//                header.forEach(headers::set);
+//            }
+//            HttpMethod method;
+//            for (DataList dataList : scenario.getData_list()) {
+//                method= HttpMethod.valueOf(dataList.getMapping_type());
+//                featureName = dataList.getScenario().getName();
+//                operationType = dataList.getMapping_type();
+//                header = dataList.getHeader();
+//                test = extent.createTest("Feature: " + featureName)
+//                        .assignCategory("API Testing")
+//                        .assignAuthor("Automation Team");
+//                for (DataSet dataset : dataList.getScenario().getDatasets()) {
+//                    Map<String, Object> requestBody = dataset.getRequest_body() != null
+//                            ? dataset.getRequest_body()
+//                            : new HashMap<>();
+//                    HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
+//
+//                    String expectedStatus = dataset.getDesired_status() != null ? dataset.getDesired_status() : "";
+//                    String expectedResponse = dataset.getDesired_outcome() != null ? dataset.getDesired_outcome() : "{}";
+//                    String endpoint = dataList.getBase_url()+dataList.getEndPoint();
+//                    // Converting actual and expected responses to Maps for easy comparison
+//                    ObjectMapper objectMapper = new ObjectMapper();
+//                    test.info(endpoint);
+//                    test.info(String.valueOf(method));
+//
+////                    Map<String, Object> expectedResponseMap = objectMapper.readValue(expectedResponse, Map.class);
+////                    String expectedResponseBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(expectedResponse);
+////                   String finalExpectedResponse =  objectMapper.writeValueAsString(expectedResponseBody);
+//
+//                    try {
+//                        response = restTemplate.exchange(endpoint, method, entity, String.class);
+//                        String actualResponse = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response.getBody());
+////
+////                        Map<String, Object> actualResponseMap = objectMapper.convertValue(response.getBody(), Map.class);
+//
+////                        if (response.getStatusCodeValue() == Integer.parseInt(dataset.getDesired_status()) &&
+////                                actualResponse.equals(expectedResponse)) {
+//                            if (response.getStatusCodeValue() == Integer.parseInt(dataset.getDesired_status())) {
+//                            test.pass("✅ Status Matched: " + expectedStatus);
+//                        } else {
+//                            test.fail("⚠️ Unexpected Status: Expected " + dataset.getDesired_status() + " but got " + response.getStatusCodeValue());
+////                            test.fail("Unexpected Status: Expected Response Body " + dataset.getDesired_status() + " but got " + response.getBody());
+//                        }
+//
+//                    } catch (Exception e) {
+//                        // Log the failure and continue with the next dataset
+//                        test.fail("❌ API call failed for " + dataset.getRequest_body() + ": " + e.getMessage());
+//                    }
+//                }
+//    }
+//            extent.flush();
+//
+//
+//            try {
+//                String htmlContent = new String(Files.readAllBytes(Paths.get("APIReport.html")), StandardCharsets.UTF_8);
+//                scenario.setReport(htmlContent);
+//            } catch (IOException e) {
+//                System.err.println("Failed to read Extent Report: " + e.getMessage());
+//            }
+//
+//         String finalMessage=  objectMapper.writeValueAsString(scenario);
+//
+//                String responseTopic = "QATransaction_API_REPORT";
+//                kafkaTemplate.send(responseTopic, finalMessage);
+//                System.out.println("Published response to Kafka topic: " + responseTopic);
+//
+//            File file = new File("APIReport.html");
+//            file.delete();
+//        }
 
 
     private static void setupExtentReports() {
